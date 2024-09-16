a23d07804f152764ddee55758abedae58e673e89
Kyle Swarner <kyds@parkourpizza.com>
2024-09-16 15:55:06 -0400

    Increased Puzzle Image Size
    To support zooming, the puzzle image is rendered at a higher quality than is typically needed. I noticed that game images were still looking quite blurry when zoomed in, so this change increases the size of the image to be 2048px on square (up from 1024). If this causes performance issues on some devices, I may need to consider making this a setting.

--- Puzzles Reloaded/Puzzles Reloaded/Utils/PuzzleConstants.swift
+++ Puzzles Reloaded/Puzzles Reloaded/Utils/PuzzleConstants.swift
@@ -10,7 +10,7 @@ import Foundation
 
 struct PuzzleConstants {
     
-    static let puzzleSize = 1024 // Given to the puzzle midend & our CGContext to build the image. This needs to be consistent across several areas in the app!
+    static let puzzleSize = 2048 // Given to the puzzle midend & our CGContext to build the image. This needs to be consistent across several areas in the app!
     static let PencilDragDeadzone = 15.0 //Ignore drags from pencils until they are this far away from origin. Allows for more consistent long press functionality when using the pencil.
     
     static let settingExclusions: [String] = ["Keyboard shortcuts without Ctrl", "Numpad inputs"]
